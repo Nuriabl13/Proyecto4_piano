2e0a36d9a1f8b565d32fa7bc54d162359b592765
Raixorus <danielsimonpuertas@gmail.com>
2026-04-23 13:29:50 +0200

    tecla blanca 2 y blanca 3 terminadas

--- src/jd/piano/teclas/TeclaBlanca2.java
+++ src/jd/piano/teclas/TeclaBlanca2.java
@@ -5,4 +5,26 @@ public class TeclaBlanca2 extends TeclaBlanca{
     TeclaBlanca2(int n){
         super(n);
     }
+    @Override
+    protected int[] getVerticesX() {
+        int N = TeclaNegra.ANCHURA;
+        int B = TeclaBlanca.ANCHURA;
+        int[] x = new int[]{
+                super.posicion.x,
+                super.posicion.x+(N/2),
+                (super.posicion.x+B)-(N/2),
+                super.posicion.x+B
+        };
+        return x;
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        int[] y = new int[]{
+                super.posicion.y,
+                super.posicion.y + TeclaNegra.ALTURA,
+                super.posicion.y + TeclaBlanca.ALTURA,
+        };
+        return y;
+    }
 }
--- src/jd/piano/teclas/TeclaBlanca3.java
+++ src/jd/piano/teclas/TeclaBlanca3.java
@@ -5,4 +5,26 @@ public class TeclaBlanca3 extends TeclaBlanca{
     TeclaBlanca3(int n){
         super(n);
     }
+
+    @Override
+    protected int[] getVerticesX() {
+        int N = TeclaNegra.ANCHURA;
+        int B = TeclaBlanca.ANCHURA;
+        int[] x = new int[]{
+                super.posicion.x,
+                (super.posicion.x+B)-(N/2),
+                super.posicion.x+B
+        };
+        return x;
+    }
+
+    @Override
+    protected int[] getVerticesY() {
+        int[] y = new int[]{
+                super.posicion.y,
+                super.posicion.y + TeclaNegra.ALTURA,
+                super.posicion.y + TeclaBlanca.ALTURA,
+        };
+        return y;
+    }
 }
