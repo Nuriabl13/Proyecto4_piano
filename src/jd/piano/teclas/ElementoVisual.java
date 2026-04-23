22f8bfe5b10bf19b2d267702582cc7a0f6eea53f
jonasGarra <jonasriket@gmail.com>
2026-04-23 12:41:40 +0200

    extends ElementoVisual

--- src/jd/piano/teclas/ElementoVisual.java
+++ src/jd/piano/teclas/ElementoVisual.java
@@ -2,8 +2,9 @@ package jd.piano.teclas;
 
 import java.awt.*;
 
-public interface ElementoVisual {
+public interface ElementoVisual extends Medible {
     public void setPosicion(int x, int y);
     public void setGraphics(Graphics g);
     public void dibujar();
+
 }
