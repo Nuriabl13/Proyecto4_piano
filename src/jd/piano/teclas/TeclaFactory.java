f9f1aedde822f98801e7eb965b4fbcc25034fc5f
Nuriabl13 <nuria13balderas2l@gmail.com>
2026-04-23 13:01:24 +0200

    Clase piano corregida

--- src/jd/piano/teclas/TeclaFactory.java
+++ src/jd/piano/teclas/TeclaFactory.java
@@ -1,10 +1,18 @@
 package jd.piano.teclas;
 
-public class TeclaFactory {
+public class TeclaFactory{
     private TeclaFactory(){
 
     }
     public static Tecla crearTecla(int nota){
-        return null;
+        int not = nota%12;
+        Tecla tecla = null;
+        switch (not){
+            case 1,3,6,8,10:
+                tecla = new TeclaNegra();
+                break;
+            case 0,5:
+                tecla = new TeclaBlanca1();
+        }
     }
 }
